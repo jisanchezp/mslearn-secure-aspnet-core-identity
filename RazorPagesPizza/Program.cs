d187836f1b681e4e98cb8a0f78379818d00d2e97
Juan Ignacio Sánchez <juanisanchez@outlook.com>
2025-04-07 14:34:15 +0000

    Added identity pages

--- RazorPagesPizza/Program.cs
+++ RazorPagesPizza/Program.cs
@@ -6,7 +6,8 @@ var connectionString = builder.Configuration.GetConnectionString("RazorPagesPizz
 
 builder.Services.AddDbContext<RazorPagesPizzaAuth>(options => options.UseSqlServer(connectionString));
 
-builder.Services.AddDefaultIdentity<RazorPagesPizzaUser>(options => options.SignIn.RequireConfirmedAccount = true).AddEntityFrameworkStores<RazorPagesPizzaAuth>();
+builder.Services.AddDefaultIdentity<RazorPagesPizzaUser>(options => options.SignIn.RequireConfirmedAccount = true)
+    .AddEntityFrameworkStores<RazorPagesPizzaAuth>();
 
 // Add services to the container.
 builder.Services.AddRazorPages();
